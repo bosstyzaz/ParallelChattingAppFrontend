c78b7b75f6a14954568b6aa69b5bf0ef0ccdd5ef
Zongpol Jongkirkkiat <levatinez@gmail.com>
2020-04-29 18:14:04 +0700

    almost done front

--- src/Components/MyGroup/Group.js
+++ src/Components/MyGroup/Group.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Group.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Group extends React.Component {
     constructor(props) {
@@ -19,9 +20,9 @@ class Group extends React.Component {
             <div className='group-container'>
                 <div className='group-name'>{this.props.groupname}</div>
                 <div>
-                    <button className='read-button' onClick={this.props.handleRead.bind(this,this.props.groupid)}>read</button>
-                    <button className='getUnread-button' onClick={this.props.handleUnread.bind(this,this.props.groupid)}>getUnread</button>
-                    <button className='leave-button' onClick={this.props.handleLeave.bind(this,this.props.groupid)}>Leave</button>
+                    <button className='read-button' onClick={this.props.handleRead.bind(this,this.props.groupid)}><FontAwesomeIcon icon={['fas', 'book-reader']} type="submit"/><br/>read</button>
+                    <button className='getUnread-button' onClick={this.props.handleUnread.bind(this,this.props.groupid)}><FontAwesomeIcon icon={['fas', 'flag-checkered']} type="submit"/><br/>getUnread</button>
+                    <button className='leave-button' onClick={this.props.handleLeave.bind(this,this.props.groupid)}><FontAwesomeIcon icon={['fas', 'heart-broken']} type="submit"/><br/>Leave</button>
                 </div>
             </div>
         );
